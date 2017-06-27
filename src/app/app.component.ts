481e387d3014a4bef21bda17c4a5fffcf7036db2
Carlos Luis <clburguete@gmail.com>
2017-06-27 15:31:58 +0200

    photo upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview'
+import { CameraPreview, CameraPreviewOptions, CameraPreviewPictureOptions } from '@ionic-native/camera-preview'
 import { HomePage } from '../pages/home/home';
 import { CameraPrevPage } from '../pages/camera-prev/camera-prev'
 import * as firebase from 'firebase'
@@ -13,8 +13,15 @@ import * as firebase from 'firebase'
 })
 export class MyApp {
   rootPage:any = CameraPrevPage;
+  picture: string = null;
+  pictureOpts: CameraPreviewPictureOptions = {
+    width: window.screen.width,
+    height: window.screen.height,
+    quality: 85,
+};
+  image:string = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public camerapreview: CameraPreview) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public camerapreview: CameraPreview, private alertCtrl: AlertController) {
     platform.ready().then(() => {
 
       // Okay, so the platform is ready and our plugins are available.
@@ -24,8 +31,8 @@ export class MyApp {
       const cameraPreviewOpts: CameraPreviewOptions = {
         x: 0,
         y: 0,
-        width: window.screen.width - 100,
-        height: window.screen.height - 100,
+        width: window.screen.width ,
+        height: window.screen.height ,
         camera: 'rear',
         tapPhoto: true,
         previewDrag: true,
@@ -53,4 +60,39 @@ export class MyApp {
       firebase.initializeApp(config);
     });
   }
+
+  takePicture(){
+    this.camerapreview.takePicture(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+      let storageRef = firebase.storage().ref();
+      const filename = Math.floor(Date.now() / 1000);
+      const imageRef = storageRef.child(`images/${filename}.jpg`);
+      imageRef.putString(this.picture, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+        this.showAlert("IMAGE UPLOADED")
+
+      });
+
+
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  upload(){
+    let storageRef = firebase.storage().ref();
+    const filename = Math.floor(Date.now() / 1000);
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+    imageRef.putString(this.image, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+      this.showAlert("IMAGE UPLOADED")
+    });
+  }
+
+  showAlert(test) {
+     let alert = this.alertCtrl.create({
+       title: test,
+       buttons: ['OK']
+     })
+     alert.present();
+   }
 }
--- src/pages/camera-prev/camera-prev.ts
+++ src/pages/camera-prev/camera-prev.ts
@@ -13,11 +13,7 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Came
   templateUrl: 'camera-prev.html',
 })
 export class CameraPrevPage {
-  pictureOpts: CameraPreviewPictureOptions = {
-    width: window.screen.width,
-    height: window.screen.height,
-    quality: 85,
-};
+
   picture: string = null;
   constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, private camerapreview: CameraPreview) {
     this.showAlert("LLEGA A LA PAGINA")
@@ -35,12 +31,12 @@ export class CameraPrevPage {
      })
      alert.present();
    }
-  takePicture(){
-    this.camerapreview.takePicture(this.pictureOpts).then((imageData) => {
-      this.picture = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      console.log(err);
-    });
-  }
+  // takePicture(){
+  //   this.camerapreview.takePicture(this.pictureOpts).then((imageData) => {
+  //     this.picture = 'data:image/jpeg;base64,' + imageData;
+  //   }, (err) => {
+  //     console.log(err);
+  //   });
+  // }
 
 }
